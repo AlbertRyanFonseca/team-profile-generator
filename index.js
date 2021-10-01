9a787dbe6f259ea5e7001bd52c3578b2ae4defd6
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-10-01 15:25:48 -0700

    Added questions throught index.js. A bit cleaner and easier to access. Answer function will display based on answer of role

--- index.js
+++ index.js
@@ -1,3 +1,64 @@
-const questionInfo = require('./src/questionFile');
+const inquirer = require("inquirer");
+const fs = require('fs');
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 
-questionInfo();
+const employeeArr = [];
+
+const question = [
+    {
+        type: 'list',
+        name: 'chooseRole',
+        message: 'What position is this employee?',
+        choices: ['Manager', 'Intern', 'Engineer']
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your id number?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'what is your email?'
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What school have you attended?',
+        when: answers => answers.roleChoice === 'Intern'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'enter your github username?',
+        when: answers => answers.rolechoice === 'Engineer'
+    },
+    {
+        type: 'input',
+        name: 'phone',
+        message: 'what is your office number?',
+        when: answers => answers.roleChoice === 'Manager'
+    }
+]
+
+function initAnswers() {
+    inquirer.prompt(question).then((answers) => {
+        if (answers.chooseRole === 'Intern') {
+            const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+            employeeArr.push(intern);
+        } else if (answers.chooseRole === 'Engineer') {
+            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            employeeArr.push(engineer);
+        } else if (answers.chooseRole === 'Manager') {
+            const manager = new Manager(answers.name, answers.id, answers.email, answers.phone);
+            employeeArr.push(manager);
+        }
+    })
+}
