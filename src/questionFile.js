b8f42cfabaae71fc1c313834819461e813c7aa97
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-10-01 15:12:13 -0700

    Added question file

--- src/questionFile.js
+++ src/questionFile.js
@@ -1,2 +1,39 @@
 const inquirer = require('inquirer');
+const Employee = require('../lib/Employee');
 
+function questionInfo() {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }])
+        .then(({ name }) => {
+            new Employee(name);
+            this.getId();
+        })
+};
+
+questionInfo.prototype.getId = function () {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'id',
+        message: 'What is your id #?'
+    }])
+        .then(({ id }) => {
+            new Employee(id);
+            this.getEmail();
+        })
+};
+
+questionInfo.prototype.getEmail = function () {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'email',
+        message: 'What is your e-mail?'
+    }])
+        .then(({ email }) => {
+            new Employee(email);
+        })
+}
+
+module.exports = questionInfo;
